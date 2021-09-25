e6451453b186cb01ba3949d6a7df92560c4cfbc6
Sagytic <jb9992@daum.net>
2021-09-25 17:23:56 +0900

    boj 2841

--- boj/greedy/b4796.py
+++ boj/greedy/b4796.py
@@ -2,15 +2,6 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-i = 1
-while True:
-    L, P, V = map(int, input().split()) # L: 사용 가능 일 / P : 연속하는 캠핑일 / V : 휴가일
-    if not L:
-        break
-    ans = 0
-    minans = V%P
-    if L < V%P:
-        minans = L
-    ans += (((V//P)*L) + minans)
-    print(f'Case {i}: {ans}')
-    i += 1
+for tc in range(1, int(input())):
+    E, N = map(int, input())
+    
