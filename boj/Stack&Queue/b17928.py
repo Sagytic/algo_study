1d4eb18e4a45aa12fb04929733031c09f1a7f68f
Sagytic <jb9992@daum.net>
2021-11-10 23:16:55 +0900

    boj 17928

--- boj/Stack&Queue/b17928.py
+++ boj/Stack&Queue/b17928.py
@@ -3,20 +3,33 @@
 
 N = int(input())
 li = list(map(int, input().split()))
-ans = []
+stack = []
+ans = [0]*N
 
-while li:
-    tmp = li.pop(0)
-    flag = True
-    if not len(li):
-        ans.append(-1)
+for i in range(N):
+    if stack:
+        if li[stack[-1]] >= li[i]:
+            stack.append(i)
+        else:
+            while stack and li[stack[-1]] < li[i]:
+                tmp = stack.pop()
+                ans[tmp] = li[i]
+            stack.append(i)
     else:
-        for i in li:
-            if tmp < i:
-                ans.append(i)
-                flag = False
-                break
-        if flag:
-            ans.append(-1)
+        stack.append(i)
 
+
+# while li:
+#     tmp = li.pop(0)
+#     flag = True
+#     if not len(li):
+#         ans.append(-1)
+#     else:
+#         for i in li:
+#             if tmp < i:
+#                 ans.append(i)
+#                 flag = False
+#                 break
+#         if flag:
+#             ans.append(-1)
 print(*ans)
--- test.py
+++ test.py
@@ -5,43 +5,20 @@ from itertools import permutations
 from collections import deque
 
 sys.stdin = open('input.txt')
-import sys
-sys.setrecursionlimit(100000)
-
-def bfs(n):
-    global ans, flag
-    tmp = crossline[n]
-    for i in tmp:
-        if i == N:
-            return
-
-        # print(i)
-        if visited[i] == 1:
-            ans = 'CYCLE'
-            flag = True
-            return
-        elif visited[i] == 0:
-            visited[i] += 1
-        if flag:
-            return
-        bfs(i)
-        visited[i] = 0
 
 N = int(input())
-visited = [0] + [0]*N
-visited[1] = 1
-ans = 'NO CYCLE'
-flag = False
-line = [list(map(int, input().split())) for _ in range((N-1)*2)]
-crossline = {}
-a = 0
-for li in range(0, (N-1)*2, 2):
-    a += 1
-    crossline[a] = line[li+1]
-# crossline[N] = []
-# for i in range(1, N):
-#     if N in crossline[i]:
-#         crossline[N] += [i]
-bfs(1)
-# print(crossline)
-print(ans)
+li = list(map(int, input().split()))
+stack = []
+ans = [-1]*N
+for i in range(N):
+    if stack:
+        if li[stack[-1]] >= li[i]:
+            stack.append(i)
+        else:
+            while stack and li[stack[-1]] < li[i]:
+                tmp = stack.pop()
+                ans[tmp] = li[i]
+            stack.append(i)
+    else:
+        stack.append(i)
+print(*ans)
